104ec2ffa1ea4fd644af25f445dc28b2f57ef33f
Thomas-Deasington <t.deasington@gmail.com>
2023-03-10 12:23:48 +0100

    add copy to clipboard

--- src/lib/components/TripDescription.svelte
+++ src/lib/components/TripDescription.svelte
@@ -5,7 +5,18 @@
     <p><mark>Vancouver</mark> + <mark>2 day road trip with kids and pets</mark></p>
     <p><mark>San Francisco</mark> + <mark>5 things to do in the city</mark></p>
   {:else}
-      <h2 class="text-xl font-bold">{@html $tripPlan.title}</h2>
+      <div class="flex justify-between">
+        <h2 class="text-xl font-bold">{@html $tripPlan.title}</h2>
+        <button
+          on:click={copyToClipboard}
+          class="hover:text-indigo-200"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+            <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+          </svg>
+        </button>
+      </div>
       <div class="my-5 border border-b-0 border-indigo-300"></div> 
       <p>{@html $tripPlan.description}</p>
       <div class="mt-5">
@@ -30,6 +41,8 @@
 
 <script lang="ts">
   import { tripPlan, selectedPointOfInterestIndex } from '$lib/stores/main';
+  import copy from 'copy-to-clipboard';
+  import { toast } from '@zerodevx/svelte-toast'
 
   $: tripPlanIsEmpty = Object.values($tripPlan).every(x => x !== null && x.length === 0)
 
@@ -37,6 +50,19 @@
     return () => {
       $selectedPointOfInterestIndex = index
     }
+  }
+
+  function copyToClipboard () {
+    const text = $tripPlan.title + '\n' + $tripPlan.description + '\n\n' + $tripPlan.pointsOfInterest.map(place => `${place.title} : ${place.description}`).join('\n')
+    copy(text);
+    toast.push('Copied to clipboard ✅', {
+      duration: 2000,
+      theme: {
+        '--toastColor': 'mintcream',
+        '--toastBackground': '#4BB543',
+        '--toastBarBackground': '#4BB543'
+      }
+    })
   }
 </script>
 
--- src/lib/components/TripMap.svelte
+++ src/lib/components/TripMap.svelte
@@ -1,12 +1,17 @@
 <div class="w-1/2 h-[500px] border border-l-0 border-white rounded-sm rounded-l-none">
   <Map accessToken={PUBLIC_MAPBOX_TOKEN}
-    style="mapbox://styles/mapbox/outdoors-v11" knekln
+    style="mapbox://styles/mapbox/outdoors-v11"
     bind:this={mapComponent} 
     on:ready={onReady}
   >
     {#each $tripPlan.pointsOfInterest as place}
       {#if place.latitude && place.longitude}
-        <Marker lat={place.latitude} lng={place.longitude} label={place.title} color="rgb(129,140,248)" />
+        <Marker
+          lat={place.latitude}
+          lng={place.longitude}
+          label={place.title}
+          color="rgb(129,140,248)"
+        />
       {/if}
     {/each}
   </Map>
@@ -15,22 +20,19 @@
 <script lang="ts">
   import { Map, Marker } from '@beyonk/svelte-mapbox'
   import { PUBLIC_MAPBOX_TOKEN } from '$env/static/public'
-  import { tripPlan, selectedPointOfInterestIndex, isTripPlanLoading } from '$lib/stores/main';
+  import { tripPlan, selectedPointOfInterestIndex } from '$lib/stores/main';
 
   let mapComponent: any
   $: selectedPointOfInterest = $tripPlan.pointsOfInterest?.[$selectedPointOfInterestIndex] || null
 
   $: if (selectedPointOfInterest) {
-    try {
-      mapComponent.setCenter([selectedPointOfInterest.longitude, selectedPointOfInterest.latitude])
-      mapComponent.flyTo({ center: [selectedPointOfInterest.longitude, selectedPointOfInterest.latitude] })
-      mapComponent.setZoom(12)
-    } catch {
-      setDefaultCenter()
-    }
+    mapComponent.setZoom(12)
+    mapComponent.flyTo({ center: [selectedPointOfInterest.longitude, selectedPointOfInterest.latitude] })
   }
 
-  // TODO: add the flyTo animation to the map and set the center only the first time
+  // TODO: Try to improve the precision of markers
+  // TODO: Add google link to the marker tooltip
+  // TODO: Add image in the place of the marker
 
   function setDefaultCenter() {
     mapComponent.setCenter([139.7521, 35.6825])
